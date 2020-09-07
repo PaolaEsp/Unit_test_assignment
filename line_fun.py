47d3f0e401893146969e4ca54586a00f8b920144
Paola Espinal <Paola@Paolas-MacBook-Pro-2.local>
2020-09-07 10:07:21 -0400

    Modified it so that the function accepts three parameters.

--- line_fun.py
+++ line_fun.py
@@ -1,7 +1,7 @@
 # line function
-def y_value((x1, y1), (x2, y2), x_val):
-    m = (y2 -y1) / (x2 -x1)
-    b = y1 - m * x1
+def y_value(a, b, x_val):
+    m = (b[1] -a[1]) / (b[0] -a[0])
+    b = a[1] - m * a[0]
     y_val = m * x_val + b
     return y_val
 

60bbd25c610fb43edc6ce9ff1f82fc8b820917d8
Paola Espinal <Paola@Paolas-MacBook-Pro-2.local>
2020-09-07 10:16:16 -0400

    Changed the variable name for less confusion.

--- line_fun.py
+++ line_fun.py
@@ -1,8 +1,8 @@
 # line function
 def y_value(a, b, x_val):
     m = (b[1] -a[1]) / (b[0] -a[0])
-    b = a[1] - m * a[0]
-    y_val = m * x_val + b
+    bint = a[1] - m * a[0]
+    y_val = m * x_val + bint
     return y_val
 
 

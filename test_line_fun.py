946938797c548ac3290507335c9c5a1a68e09156
Paola Espinal <Paola@Paolas-MacBook-Pro-2.local>
2020-09-07 10:08:43 -0400

    Made sure to include four parameters in the parametrize function and three in the test_y_value one.

--- test_line_fun.py
+++ test_line_fun.py
@@ -1,11 +1,11 @@
 import pytest
 
-@pytest.mark.parametrize("number, expected", [
-    (5, 13), # y = 2x + 3
-    (8, 19),
-    (10, 23),
+@pytest.mark.parametrize("coord1, coord2, number, expected", [
+    ((1,5), (3,9), 5, 13), # y = 2x + 3
+    ((1,-3), (2,-2), 8, 4),
+    ((1,4), (2,8), 10, 40),
     ])
-def test_y_value(number, expected):
+def test_y_value(coord1, coord2, number, expected):
     from line_fun import y_value
-    result = y_value(number)
+    result = y_value(coord1, coord2, number)
     assert result == expected
